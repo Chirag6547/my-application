8a59780e46191c9077c80725b6f04091116557f7
HP <cngolakiya4@gmail.com>
2020-07-19 18:35:21 +0530

    Hello Chirag

--- app/src/main/java/com/cloudus/myapplication/MainActivity.java
+++ app/src/main/java/com/cloudus/myapplication/MainActivity.java
@@ -3,12 +3,15 @@ package com.cloudus.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txtWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtWord = findViewById(R.id.txtWord);
     }
 }
